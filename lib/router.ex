173d127ba519d3034c127cbe0d69173e868e9a36
skovsgaard <jonas.skovsgaard.christensen@gmail.com>
2015-12-19 15:11:39 +0100

    Data improved, still not accurate

--- lib/router.ex
+++ lib/router.ex
@@ -5,11 +5,14 @@ defmodule FuturamaQuotes.Router do
   plug :match
   plug :dispatch
 
-  get "/quote", do: send_resp conn, 200, Server.get_all
-  get "/quote/:id", do: send_resp conn, 200, "[quote from the list]"
-  get "/quote/random", do: send_resp conn, 200, "[random quote]"
-  get "/quote/regex/:regex", do: send_resp conn, 200, "[quote matching regex]"
+  get "/", do: send_resp conn, 200, Server.get_all |> Poison.encode!
+  get "/quote", do: send_resp conn, 200, Server.get_all |> Poison.encode!
+  get "/quote/:id", do: send_resp conn, 200, Server.get_by_id(id) |> Poison.encode!
+  get "/quote/random", do: send_resp conn, 200, Server.get_random |> Poison.encode!
+  get "/quote/regex/:regex", do: send_resp conn, 200, Server.get_regex(regex) |> Poison.encode!
   get "/quote/by/:person", do: send_resp conn, 200, "[quotes by person]"
 
   post "/quote", do: send_resp conn, 403, "[will add person]"
+
+  match _, do: send_resp conn, 404, "No quote found for that route"
 end
--- lib/server.ex
+++ lib/server.ex
@@ -3,15 +3,13 @@ defmodule FuturamaQuotes.Server do
 
   @mod __MODULE__
 
-  defmodule Quote do
-    defstruct by: nil, text: nil
-  end
+  defmodule Quote, do: defstruct by: nil, text: nil
 
   # Client
 
   def start_link, do: GenServer.start_link(@mod, :ok, name: @mod)
-  def get_all!, do: GenServer.call(@mod, :all)
-  def get_by_id!(id), do: GenServer.call(@mod, {:id, id})
+  def get_all, do: GenServer.call(@mod, :all)
+  def get_by_id(id), do: GenServer.call(@mod, {:id, id})
   def get_random, do: GenServer.call(@mod, :random)
   def get_regex(regex), do: GenServer.call(@mod, {:regex, regex})
 
@@ -22,17 +20,13 @@ defmodule FuturamaQuotes.Server do
   end
 
   def handle_call({:id, id}, _from, state) do
-    {:reply,
-     quotes |> Enum.filter(&(&1.by == id)),
-     state}
+    {:reply, quotes |> Enum.at(id |> String.to_integer), state}
   end
 
   def handle_call(:random, _from, state) do
     {:reply,
      quotes
-     |> Enum.at(quotes
-                |> Enum.count
-                |> :random.uniform),
+     |> Enum.at(quotes |> Enum.count |> :random.uniform),
      state}
   end
 
@@ -43,31 +37,28 @@ defmodule FuturamaQuotes.Server do
   end
 
   # Private helpers
-  defp build_quotes(q_list) do
-    for entry <- q_list do
-      entry
-      |> String.split("\n")
-      |> build_entry
+  defp build_quotes([h|t]) do
+    cond do
+      String.at(h,0) == "“" -> do_single_quote([h|t], [])
+      String.match?(h, ~r/^[^" ,]+.+: /) -> do_multi_quote([h|t], [])
+      true -> %Quote{text: Enum.join(t)}
     end
   end
 
-  defp build_entry(entry) do
-    cond do
-      String.match?(List.first(entry), ~r/^[^ ]*: /) ->
-        [h|t] = entry |> List.first |> String.split(": ")
-        %Quote{by: h, text: t |> Enum.join}
-      String.match?(List.first(entry), ~r/^“/) ->
-        %Quote{by: entry |> List.last |> String.split(" -") |> List.last,
-               text: entry}
-      true -> %Quote{text: Enum.join(entry)}
-    end
+  defp do_multi_quote([], acc), do: acc
+  defp do_multi_quote([h|t], acc) do
+    [character|text] = h |> String.split(":")
+    do_multi_quote(t, [acc | %Quote{by: character, text: text |> Enum.join}])
   end
+
+  defp do_single_quote([h|t], acc), do: :ok
 
   def quotes do
     "#{System.cwd!}/futurama.json"
       |> File.read!
       |> Poison.decode!
-      |> build_quotes
+      |> Enum.map(&(&1 |> String.split("\n")))
+      |> Enum.map(&build_quotes/1)
   end
 end
     
